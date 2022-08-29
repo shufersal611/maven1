78d056697a739cb694c0b64e51572d22c633f73d
שרקייה <m_nmh@hotmail.com>
2022-08-29 12:17:00 +0300

    שינוי חדש

--- src/test/java/trkol.java
+++ src/test/java/trkol.java
@@ -1,12 +1,22 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class trkol {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException, InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.google.co.il/");
+        driver.get("https://www.google.co.il");
+        Thread.sleep(2000);
+        driver.findElement(By.className("gb_Ue")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"yDmH0d\"]/c-wiz/div/div/c-wiz/div/div/ul[1]/li[10]/a/div[5]/span")).click();
+     //   Robot robot = new Robot();
+      //  robot.keyPress(KeyEvent.VK_H);
     }
 }
