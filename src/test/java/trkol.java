2e5ab6c258c22fc7f95e93765f7b532ff1f1cb1f
שרקייה <m_nmh@hotmail.com>
2022-08-29 11:56:14 +0300

    Initial commit

---
+++ src/test/java/trkol.java
@@ -0,0 +1,12 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class trkol {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.co.il/");
+    }
+}
